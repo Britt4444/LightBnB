d90cab522ccb11c847c5b8ff167d58a9212d6d44
Brittany Cowper <brittferren@gmail.com>
2023-05-19 15:37:14 -0500

    eslint database.js, add DrawSQL ERD to docs

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,11 +1,8 @@
-
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
-
+// access environment variables
 const dotenv = require("dotenv");
 dotenv.config();
 
-
+// create connection pool
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -24,7 +21,7 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 
-const getUserWithEmail = function (email) {
+const getUserWithEmail = function(email) {
   const queryString = `
   SELECT * FROM users 
   WHERE email = $1;
@@ -46,7 +43,7 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
+const getUserWithId = function(id) {
   const queryString = `
   SELECT * FROM users 
   WHERE id = $1;
@@ -89,7 +86,7 @@ const addUser = function(user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) { // this is not working still!!
+const getAllReservations = function(guest_id, limit = 10) { // this is not working still!!
   const queryString = `
   SELECT reservations.*, title, thumbnail_photo_url, number_of_bathrooms, number_of_bedrooms,
   parking_spaces, cost_per_night, AVG(property_reviews.rating) as average_rating
@@ -146,7 +143,7 @@ const getAllProperties = (options, limit = 10) => {
   if (options.minimum_rating) {
     const operator = queryParams.length > 0 ? 'AND' : 'WHERE';
     queryParams.push(options.minimum_rating);
-    queryString += `${operator} rating >= $${queryParams.length}`
+    queryString += `${operator} rating >= $${queryParams.length}`;
   }
 
   queryParams.push(limit);
@@ -170,15 +167,15 @@ const getAllProperties = (options, limit = 10) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
+const addProperty = function(property) {
   const queryString = `
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province,
   post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
   RETURNING *;`;
   const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
-  property.cost_per_night * 100, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces,
-  property.number_of_bathrooms, property.number_of_bedrooms];
+    property.cost_per_night * 100, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces,
+    property.number_of_bathrooms, property.number_of_bedrooms];
 
   return pool
     .query(queryString, queryParams)
